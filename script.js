98a69aa7e96965f7c99ca62b47653274f97c0797
Aurelien229 <blampainaurelien229@gmail.com>
2023-12-15 11:59:49 +0100

    corrected

--- script.js
+++ script.js
@@ -9,18 +9,20 @@ let lastChild = olElement.lastElementChild;
 // Déplacer le dernier enfant au début de la liste
 olElement.insertBefore(lastChild, olElement.firstChild);
 
+// Sélectionner tous les éléments <section>
+let sections = document.querySelectorAll('main section');
 
-// Sélectionner les balises <h2> des troisième et deuxième sections
-let h2ThirdSection = document.querySelector('section:nth-child(3) h2');
-let h2SecondSection = document.querySelector('section:nth-child(2) h2');
+// Vérifier s'il y a au moins 3 sections
+if (sections.length >= 3) {
+// Récupérer les <h2> des sections 2 et 3
+let h2Section2 = sections[1].querySelector('h2');
+let h2Section3 = sections[2].querySelector('h2');
 
-// Déplacer les <h2> entre les sections
-h2ThirdSection.parentNode.insertBefore(h2ThirdSection, document.querySelector('section:nth-child(2)'));
-h2SecondSection.parentNode.insertBefore(h2SecondSection, document.querySelector('section:nth-child(3)'));
+// Échanger les textes des h2 pour échanger leur contenu
+let temp = h2Section2.textContent;
+h2Section2.textContent = h2Section3.textContent;
+h2Section3.textContent = temp;
+}
 
-
-// Sélectionner la dernière section
 let lastSection = document.querySelector('section:last-child');
-
-// Supprimer la dernière section
 lastSection.parentNode.removeChild(lastSection);
